de7ff5cdc9e9293493afe5d6b10d7268bbe83b68
szmulder <bo.ma@myob.com>
2018-04-24 17:03:32 +1200

    Add Test project

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using DotNetApiGatewayIam;
+using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            var request = new AwsApiGatewayRequest()
+            {
+                RegionName = "ap-southeast-2",
+                Host = "ApiGateway-url",
+                AccessKey = "Your-AccessKey",
+                SecretKey = "Your-SecretKey",
+                AbsolutePath = "Segments",
+                JsonData = "data",
+                RequestMethod = "POST"
+            };
+            var apiRequest = new ApiRequest(request);
+            var response = apiRequest.GetPostResponse();
+
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
